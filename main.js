d44e334cc8a8e01660f57aa036f93d55579ff476
admpilot <adamdale2000@aol.com>
2023-04-29 07:32:56 -0400

    better location for variable declaration

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const companies = ['google', 'facebook', 'microsoft', 'apple'];
 const testCharacter='o';
-let countOccurrences=0;
 
 for (let i = 0; i < companies.length; i++)
 {
+    let countOccurrences=0;
+
     for (let x = 0; x < companies[i].length; x++)
     {
         if (companies[i][x]==testCharacter)
